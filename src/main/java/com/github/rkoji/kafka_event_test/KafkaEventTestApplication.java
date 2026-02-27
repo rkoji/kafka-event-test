5cbf590966473d71e35573ae8039947288358fae
rkoji <wl0wn1212@gmail.com>
2026-02-27 14:23:52 +0900

    Initial commit

---
+++ src/main/java/com/github/rkoji/kafka_event_test/KafkaEventTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.rkoji.kafka_event_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEventTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEventTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rkoji/kafka_event_test/KafkaEventTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rkoji.kafka_event_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEventTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
